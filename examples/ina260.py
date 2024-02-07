9a3504a58cd7c35d3278f810d43016ed4e0fd7e8
adam-hrvth <adam.horvath@semify-eda.com>
2024-02-07 14:19:11 +0100

    Fixed conversion error in ina260 example script

--- examples/ina260.py
+++ examples/ina260.py
@@ -11,12 +11,12 @@ def meas_voltage(i2c):
     Read out the content of the Bus Voltage Register (0x02) and convert it into decimal format.
     Display the measured voltage in mV.
     """
-    lsb_volt = 1.25
+    lsb_volt = 1.25e-3
     i2c.write(0x40, [0x02])
     volt_read = i2c.readRegister(0x40, [0x02], 2)
-    print(f"Vbus Register content: {bin(volt_read[0] + volt_read[1])[2:].zfill(8)}")
-    volt_val = lsb_volt * (volt_read[0] + volt_read[1])
-    print(f"Measured voltage: {volt_val:.2f}mV")
+    print(f"Vbus Register content: {bin(int.from_bytes(volt_read, 'big', signed=False))}")
+    volt_val = lsb_volt * (int.from_bytes(volt_read, 'big', signed=False))
+    print(f"Measured voltage: {volt_val:.2f}V")
 
 
 def meas_current(i2c):
@@ -24,12 +24,12 @@ def meas_current(i2c):
     Read out the content of the Current Register (0x01) and convert it into decimal format.
     Display the calculate current in mA.
     """
-    lsb_current = 1.25
+    lsb_current = 1.25e-3
     i2c.write(0x40, [0x01])
     current_read = i2c.readRegister(0x40, [0x01], 2)
-    print(f"Current Register content: {bin(current_read[0] + current_read[1])[2:].zfill(8)}")
-    current_val = lsb_current * (current_read[0] + current_read[1])
-    print(f"Measured current: {current_val:.2f}mA")
+    print(f"Current Register content: {bin(int.from_bytes(current_read, 'big', signed=False))}")
+    current_val = lsb_current * (int.from_bytes(current_read, 'big', signed=True))
+    print(f"Measured current: {current_val:.2f}A")
 
 
 def meas_power(i2c):
@@ -37,12 +37,12 @@ def meas_power(i2c):
     Read out the content of the Power Register (0x03) and convert it into decimal format.
     Display the calculated power in mW.
     """
-    lsb_power = 10
+    lsb_power = 10e-3
     i2c.write(0x40, [0x03])
     power_read = i2c.readRegister(0x40, [0x03], 2)
-    print(f"Power Register content: {bin(power_read[0] + power_read[1])[2:].zfill(8)}")
-    power_val = lsb_power * (power_read[0] + power_read[1])
-    print(f"Measured power: {power_val:.2f}mW")
+    print(f"Power Register content: {bin(int.from_bytes(power_read, 'big', signed=False))}")
+    power_val = lsb_power * (int.from_bytes(power_read, 'big', signed=False))
+    print(f"Measured power: {power_val:.2f}W")
 
 
 def main():
