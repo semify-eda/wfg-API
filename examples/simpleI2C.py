12f94d5166f93c1c02cb7a4edfce6bf51b360388
Christoph Royer <mail@christophroyer.com>
2024-02-08 09:06:25 +0100

    Check config deletion

--- examples/simpleI2C.py
+++ examples/simpleI2C.py
@@ -5,30 +5,49 @@ import time
 def main():
 
     start = time.time()
-    with SmartWave() as sw:
-            with sw.createI2CConfig("A2", "A1") as i2c:
-                # sw.debugCallback = lambda x: print(x)
-                sw.connect()
+    with SmartWave().connect() as sw:
+        sw.createI2CConfig("A1", "A2")
+        with sw.createI2CConfig("A8", "A7") as i2c:
+            # sw.debugCallback = lambda x: print(x)
 
-                i2c.write(0x20, [0xaa, 0x55])
-                print("first: ", i2c.read(0x20, 2))
-                i2c.write(0x20, [0x55, 0xaa])
-                print("second: ", i2c.read(0x20, 2))
-                i2c.write(0x20, [0xff, 0xff])
-                print("thirst: ", i2c.read(0x20, 2))
+            i2c.write(0x20, [0xaa, 0x55])
+            print("first: ", i2c.read(0x20, 2))
+            i2c.write(0x20, [0x55, 0xaa])
+            print("second: ", i2c.read(0x20, 2))
+            i2c.write(0x20, [0xff, 0xff])
+            print("thirst: ", i2c.read(0x20, 2))
 
-                sw.disconnect()
-                # SmartWave().connect("COM19")
-                # SmartWave().connect("COM10")
-                # SmartWave().connect()
-                # sw.connect()
-                #
-                # i2c.write(0x20, [0xaa, 0x55])
-                # print("first: ", i2c.read(0x20, 2))
-                # i2c.write(0x20, [0x55, 0xaa])
-                # print("second: ", i2c.read(0x20, 2))
-                # i2c.write(0x20, [0xff, 0xff])
-                # print("thirst: ", i2c.read(0x20, 2))
+
+        with sw.createI2CConfig("A8", "A7") as i2c:
+            # sw.debugCallback = lambda x: print(x)
+
+            i2c.write(0x20, [0xaa, 0x55])
+            print("first: ", i2c.read(0x20, 2))
+            i2c.write(0x20, [0x55, 0xaa])
+            print("second: ", i2c.read(0x20, 2))
+            i2c.write(0x20, [0xff, 0xff])
+            print("thirst: ", i2c.read(0x20, 2))
+
+        with sw.createI2CConfig("A9", "A7") as i2c:
+            # sw.debugCallback = lambda x: print(x)
+
+            i2c.write(0x20, [0xaa, 0x55])
+            print("first: ", i2c.read(0x20, 2))
+            i2c.write(0x20, [0x55, 0xaa])
+            print("second: ", i2c.read(0x20, 2))
+            i2c.write(0x20, [0xff, 0xff])
+            print("thirst: ", i2c.read(0x20, 2))
+
+        with sw.createI2CConfig("A8", "A7") as i2c:
+            # sw.debugCallback = lambda x: print(x)
+
+            i2c.write(0x20, [0xaa, 0x55])
+            print("first: ", i2c.read(0x20, 2))
+            i2c.write(0x20, [0x55, 0xaa])
+            print("second: ", i2c.read(0x20, 2))
+            i2c.write(0x20, [0xff, 0xff])
+            print("thirst: ", i2c.read(0x20, 2))
+
     print("elapsed time: ", time.time() - start)
 
 
