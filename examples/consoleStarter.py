666c30e759445b52bb9185b7c5d5ec6547dad6bb
Christoph Royer <mail@christophroyer.com>
2024-05-02 17:35:03 +0200

    Add firmware 2.1.0 and fpga 2.1.1

--- examples/consoleStarter.py
+++ examples/consoleStarter.py
@@ -1,6 +1,5 @@
 from SmartWaveAPI import SmartWave
 
 sw = SmartWave().connect()
-# sw.debugCallback = lambda x: print(x)
-# i2c = sw.createI2CConfig(sda_pin_name="A2", scl_pin_name="A3")
-spi = sw.createSPIConfig(miso_pin_name="B1", mosi_pin_name="B2", cs_pin_name="B3", sclk_pin_name="B4", clock_speed=100e3)
+sw.debugCallback = lambda x: print(x)
+i2c = sw.createI2CConfig(sda_pin_name="A4", scl_pin_name="A3")
